55775a04711735264926558267a0d2a560c5b382
Grant Regimbal <regimbal.grant@gmail.com>
2017-08-18 21:34:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,8 @@
 def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts " #{board[0]} | #{board[1]} | #{board[2]} " #row 1
   puts "-----------" #line 1
-  puts " #{board[0]} | #{board[1]} | #{board[2]} " #row 2
+  puts " #{board[3]} | #{board[4]} | #{board[5]} " #row 2
   puts "-----------" #line 2
-  puts " #{board[0]} | #{board[1]} | #{board[2]} " #row 3
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " #row 3
 
 end
